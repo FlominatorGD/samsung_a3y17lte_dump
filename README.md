e47935e6a0846c9e8f1dd3d31f96b841cbe0418c
FlominatorGD <github.flominatogd@gmail.com>
2022-10-01 14:14:53 +0200

    Add extras for a3y17ltexc-user 8.0.0 R16NW A320FLXXS9CTK1 release-keys
    Signed-off-by: FlominatorGD <github.flominatogd@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## a3y17ltexc-user 8.0.0 R16NW A320FLXXS9CTK1 release-keys
+- Manufacturer: samsung
+- Platform: exynos5
+- Codename: a3y17lte
+- Brand: samsung
+- Flavor: a3y17ltexc-user
+- Release Version: 8.0.0
+- Id: R16NW
+- Incremental: A320FLXXS9CTK1
+- Tags: release-keys
+- CPU Abilist: armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: hu-HU
+- Screen Density: 320
+- Fingerprint: samsung/a3y17ltexc/a3y17lte:8.0.0/R16NW/A320FLXXS9CTK1:user/release-keys
+- OTA version: 
+- Branch: a3y17ltexc-user-8.0.0-R16NW-A320FLXXS9CTK1-release-keys
+- Repo: samsung_a3y17lte_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat system/priv-app/SHealth5/SHealth5.apk.* 2>/dev/null >> system/priv-app/SHealth5/SHealth5.apk
+rm -f system/priv-app/SHealth5/SHealth5.apk.* 2>/dev/null
+cat system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/priv-app/Velvet/Velvet.apk
+rm -f system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null >> system/priv-app/StickerFaceAR/StickerFaceAR.apk
+rm -f system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null
+cat system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/priv-app/SecSettings/SecSettings.apk
+rm -f system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/app/SBrowser_7.0/SBrowser_7.0.apk.* 2>/dev/null >> system/app/SBrowser_7.0/SBrowser_7.0.apk
+rm -f system/app/SBrowser_7.0/SBrowser_7.0.apk.* 2>/dev/null
